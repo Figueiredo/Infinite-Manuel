3a6f3853c30d2ede2ed9209214b22bb596fcaa1d
Ricardo Figueiredo <rjfigueiredo@netcabo.pt>
2011-04-25 17:49:43 +0000

    added Win Scene

---
+++ src/scenes/WinScene.js
@@ -0,0 +1,44 @@
+function scene_basic(compJogo)
+{
+    
+    var jogo=compJogo;
+    var tick=0;
+    var scrollMessage = "Thank you for saving me, Manuel!";    
+
+    //array de input da cena
+    var teclas = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+
+    //função para mudar o estado de teclas de input na cena
+    this.toggleKey=function(key,isPressed)
+    {
+        teclas[key]=isPressed;   
+    };
+
+    //inicializa a cena
+    this.init=function()
+    {
+        
+    };
+
+    //actualiza a cena
+    this.update = function()
+    {
+        tick++;
+        if (teclas[manuelC.KEY_JUMP])
+        {
+            jogo.toTitle();
+        }
+    };
+
+    //desenha a cena  
+    this.render= function(ctx,alfa)
+    {
+        ctx.fillStyle = '#8080a0';
+        ctx.fillRect(0, 0, 320, 240);   
+        //g.drawImage(Art.endScene[tick/24%2][0], 160-48, 100-48, null);
+        ctx.drawImage(Art.img[Art.end][tick/24%2][0],0,0,96,96, 160-48, 100-48, 96,96);
+        //drawString(g, scrollMessage, 160-scrollMessage.length()*4, 160, 0);        
+        
+    };
+    
+}
